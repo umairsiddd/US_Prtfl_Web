2426c7189d53573bca93f6d6f7358243ea691d0a
Umair Siddiqui <umairsiddd@users.noreply.github.com>
2026-07-07 21:12:54 +0500

    Force production mode in server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const { createServer } = require('http');
 const { parse } = require('url');
 const next = require('next');
 
-const dev = process.env.NODE_ENV !== 'production';
+const dev = false; // Always run in production mode on Hostinger
 const hostname = 'localhost';
 const port = process.env.PORT || 3000;
 
